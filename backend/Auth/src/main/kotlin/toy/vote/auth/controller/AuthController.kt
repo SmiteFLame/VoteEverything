a07a877f6059928e2e02423ad0f78e8a6a5cc021
SmiteFLame <lukigreen@naver.com>
2021-10-13 12:37:17 +0900

    ADD: 회원 가입

--- backend/Auth/src/main/kotlin/toy/vote/auth/controller/AuthController.kt
+++ backend/Auth/src/main/kotlin/toy/vote/auth/controller/AuthController.kt
@@ -3,10 +3,8 @@ package toy.vote.auth.controller
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.bind.annotation.*
 import toy.vote.auth.exception.UserException
 import toy.vote.auth.datasource.user.entity.User
 import toy.vote.auth.datasource.user.repository.UserRepository
@@ -15,12 +13,11 @@ import toy.vote.auth.service.UserService
 @RestController
 @RequestMapping("/auth")
 class AuthController {
-
-    @Autowired
-    lateinit var userRepository: UserRepository
-
     @Autowired
     lateinit var userService: UserService
+
+    @Autowired
+    lateinit var passwordEncoder: PasswordEncoder
 
     @PostMapping("/token")
     fun getToken() : ResponseEntity<String>{
--- backend/Auth/src/main/kotlin/toy/vote/auth/datasource/user/repository/UserRepository.kt
+++ backend/Auth/src/main/kotlin/toy/vote/auth/datasource/user/repository/UserRepository.kt
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface UserRepository :JpaRepository<User, String>{
-    fun findUsers()
     fun findUserByEmail(email : String) : User?
 }
