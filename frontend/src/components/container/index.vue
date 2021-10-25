45371205395f1fed6f46ce58b777fe2961c1f62c
SmiteFLame <lukigreen@naver.com>
2021-10-25 15:05:52 +0900

    ADD: 투표 전체 출력

--- frontend/src/components/container/index.vue
+++ frontend/src/components/container/index.vue
@@ -1,4 +1,43 @@
 <template>
 	<div>
+		<div class="container mt-3">
+  			<h2>Vote</h2>
+  			<div class="card" style="width:400px; float:left" v-for="(vote, index) in votes" :key="index">
+    			<div class="card-header">{{vote.vote.voteName}}</div>
+    			<div class="card-body" style="width:400px" v-for="(voteColumn, index) in vote.voteColumnOutputList" :key="index">
+    				{{voteColumn.voteColumn.columnName}}
+  				</div>
+    			<div class="card-footer">{{vote.vote.email}}</div>
+  			</div>
+		</div>
 	</div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default{
+	data(){
+		return{
+			votes:[],
+			limit:10,
+			offset:0
+		}
+	},
+	methods:{
+		getVotes(){
+			axios
+			.get(`http://localhost:8888/votes?limit=${this.limit}&offset=${this.offset}`)
+			.then((res) =>{
+				this.votes = res.data
+				console.log(this.votes)
+			})
+		}
+	}, created(){
+		this.getVotes()
+	}
+
+}
+
+
+</script>
