e480c2400266753a593396279268c57e2c1493c8
SmiteFLame <lukigreen@naver.com>
2021-10-26 13:47:25 +0900

    ADD: Community Server Init

---
+++ backend/Communitiy/src/main/kotlin/toy/vote/communitiy/CommunitiyApplication.kt
@@ -0,0 +1,11 @@
+package toy.vote.communitiy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommunitiyApplication
+
+fun main(args: Array<String>) {
+    runApplication<CommunitiyApplication>(*args)
+}
---
+++ backend/Communitiy/src/test/kotlin/toy/vote/communitiy/CommunitiyApplicationTests.kt
@@ -0,0 +1,13 @@
+package toy.vote.communitiy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommunitiyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
