2a40915955d812d891a15266cd9904225c2d469c
SmiteFLame <lukigreen@naver.com>
2021-11-26 15:09:21 +0900

    FIX(FE): Community 테이블 디자인 변경

--- frontend/src/components/container/community/community.vue
+++ frontend/src/components/container/community/community.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="container">
     <h4 class="text-center">커뮤니티</h4>
-	<br><br>
+	  <br><br>
 		<div class="row text-center"> 
 			<div class="col-sm-1"/>
 			<div class="col-sm-2">
-			<select class="form-control" v-model="id_type" id="id_type">
-        <option value="" selected>전체 검색</option>
-        <option value="communityId">번호</option>
-        <option value="title">제목</option>
-        <option value="name">작성자</option>
-        <option value="content">내용</option>
-      </select>
+			  <select class="form-control" v-model="id_type" id="id_type">
+          <option value="" selected>전체 검색</option>
+          <option value="communityId">번호</option>
+          <option value="title">제목</option>
+          <option value="name">작성자</option>
+          <option value="content">내용</option>
+        </select>
 			</div>
       <div class="col-sm-7">
 			  <input class="form-control" placeholder="Enter search word" type="text" v-model="id_word"/>
@@ -23,29 +23,31 @@
         <button class="btn btn-light" v-if="username" @click="movePage">등록</button>
 			</div>
 		</div>
-	<br><br>
+	  <br><br>
     <div v-if="communitys.length > 0">
-      <table class="table">
+      <table class="table" style = "text-align: center">
         <colgroup>
           <col width="15%" />
           <col width="50%" />
           <col width="15%" />
           <col width="20%" />
         </colgroup>
-        <tr>
+        <thead>
           <th>번호</th>
           <th>제목</th>
           <th>작성자</th>
           <th>날짜</th>
-        </tr>
-        <tr v-for="(community, index) in communitys" :key="index">
-          <td>{{ community.communityId }}</td>
-          <td>
-            <router-link :to="'communitysearch?no=' + community.no">{{ community.title }}</router-link>
-          </td>
-          <td v-text="community.name"></td>
-          <td v-text="timeChange(community.time)"></td>
-        </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(community, index) in communitys" :key="index">
+            <td>{{ community.communityId }}</td>
+            <td>
+              <router-link style="text-decoration:none; color:black" :to="'communitysearch?no=' + community.no">{{ community.title }}</router-link>
+            </td>
+            <td v-text="community.name"></td>
+            <td style = "text-align: left" v-text="timeChange(community.time)"></td>
+          </tr>
+        </tbody>
       </table>
     </div>
     <div v-else>게시글이 없습니다.</div>
